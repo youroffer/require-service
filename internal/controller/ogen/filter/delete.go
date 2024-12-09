d82a98a73aea64c66cb0de95b0dba31e8b29b97a
Viktor <131404990+ViktorMurman12@users.noreply.github.com>
2024-12-09 21:06:32 +0200

    Update delete.go

--- internal/controller/ogen/filter/delete.go
+++ internal/controller/ogen/filter/delete.go
@@ -10,11 +10,13 @@ import (
 
 func (h *Handler) V1AdminFiltersFilterIDDelete(ctx context.Context, params api.V1AdminFiltersFilterIDDeleteParams) (api.V1AdminFiltersFilterIDDeleteRes, error) {
 	err := h.uc.Delete(ctx, params.FilterID)
+	
 	switch {
 	case errors.Is(err, repoerr.ErrFilterNotFound):
 		return &api.V1AdminFiltersFilterIDDeleteNotFound{Message: err.Error()}, nil
 	case err != nil:
 		return nil, err
 	}
+	
 	return &api.V1AdminFiltersFilterIDDeleteNoContent{}, nil
 }
