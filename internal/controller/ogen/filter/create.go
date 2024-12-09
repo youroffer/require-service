8a93156afbfa16ca99afe277f6fe265774969f5e
Viktor <131404990+ViktorMurman12@users.noreply.github.com>
2024-12-09 21:06:54 +0200

    Update create.go

--- internal/controller/ogen/filter/create.go
+++ internal/controller/ogen/filter/create.go
@@ -11,11 +11,13 @@ import (
 
 func (h *Handler) V1AdminFiltersPost(ctx context.Context, req *api.V1AdminFiltersPostReq) (api.V1AdminFiltersPostRes, error) {
 	filter, err := h.uc.Create(ctx, req.GetWord())
+	
 	switch {
 	case errors.Is(err, repoerr.ErrFilterExist):
 		return &api.V1AdminFiltersPostConflict{Message: err.Error()}, nil
 	case err != nil:
 		return nil, err
 	}
+	
 	return entity.ConvertFilterToApi(filter), nil
 }
